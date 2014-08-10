a7361cba75cf0ac7398089f739cf883bb23b733b
Noah Manger <noah.manger@gsa.gov>
2014-08-09 21:26:00 -0700

    Initializing sliders

--- static/js/init.js
+++ static/js/init.js
@@ -10,3 +10,49 @@ $(".nav-toggle").click(function() {
   $(".main-nav").toggleClass("active");
   $(".site-title").toggle();
 });
+
+//// jQuery noUISlider implementation
+//// Documentation: http://refreshless.com/nouislider/
+$(document).ready(function(){
+  // First slider
+  $('#input-1').noUiSlider({
+    start: 25,
+    step: 1,
+    range: {
+      'min': 0,
+      'max': 100
+    }
+  });
+
+  // Second slider
+  $('#input-2').noUiSlider({
+    start: 10,
+    step: 1,
+    range: {
+      'min': 0,
+      'max': 60
+    }
+  });
+
+  // Set the displayed values
+  var value1, value1, result;
+  value1 = $('#input-1').val();
+  value2 = $('#input-2').val();
+
+  function setValues(){
+    $('#input-1-value').html('$' + value1);
+    $('#input-2-value').html(value2 + ' years');
+    $('#calculator-result-value').html('$' + (value1 * value2 * 12));
+  }
+
+  // Set the values on page load to the defaults
+  setValues();
+
+  // Multiply the values of the two sliders
+  $('.calculator-input').change(function(){
+    value1 = $('#input-1').val();
+    value2 = $('#input-2').val();
+    setValues();
+  })
+
+})
