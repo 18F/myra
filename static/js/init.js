3a31aac6bebb3c5aee967fab9459924d587adc17
Noah Manger <noah.manger@gsa.gov>
2014-08-13 13:22:18 -0700

    Moving back outside document ready

--- static/js/init.js
+++ static/js/init.js
@@ -1,20 +1,21 @@
 // Quick and Easy Javascript Detection
 $("html").removeClass( "no-js" );
 
+// Navigation
+//// Add the mobile nav option
+$(".site-header").prepend('<button type="button" class="nav-toggle btn btn-default"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="sr-only">Toggle Navigation</span></button>');
+
+//// Activate nav on mobile nav toggle click
+$(".nav-toggle").click(function() {
+  $(".main-nav").toggleClass("active");
+  $(".site-title").toggle();
+});
 
 
 
 $(document).ready(function(){
-  // Navigation
-  //// Add the mobile nav option
-  $(".site-header").prepend('<button type="button" class="nav-toggle btn btn-default"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="sr-only">Toggle Navigation</span></button>');
 
-  //// Activate nav on mobile nav toggle click
-  $(".nav-toggle").click(function() {
-    $(".main-nav").toggleClass("active");
-    $(".site-title").toggle();
-  });
-  
+
   // Tabs
   $('.tab').click(function(e){
     e.preventDefault();
