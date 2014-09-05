905dc2902a7cff748a9083245688f693c8bb897d
Noah Manger <noah.manger@gsa.gov>
2014-09-04 18:00:14 -0700

    New calculator implementation

--- static/js/calculator-v2.js
+++ static/js/calculator-v2.js
@@ -2,12 +2,12 @@
 //// Documentation: http://refreshless.com/nouislider/
 //// Calculator v2
 Number.prototype.formatMoney = function(c, d, t){
-var n = this, 
-    c = isNaN(c = Math.abs(c)) ? 2 : c, 
-    d = d == undefined ? "." : d, 
-    t = t == undefined ? "," : t, 
-    s = n < 0 ? "-" : "", 
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
     j = (j = i.length) > 3 ? j % 3 : 0;
    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
  };
@@ -35,75 +35,55 @@ $(document).ready(function(){
     }
   });
 
-  $('#input-3-slider').noUiSlider({
-    start: 35,
-    step: 1,
-    range: {
-      'min': 0,
-      'max': 100
-    },
-    serialization: {
-      lower: [
-        $.Link({
-          target: $('#input-3')
-        })
-      ],
-      format: {
-        decimals: 0,
-        thousand: ' ',
-        prefix: '$'
-      }
-    }
-  });
-
   // Set the displayed values
-  var value1, value2, result1, result2, untilRetirement;
+  var value1, value2, value3, yearsBase, yearsReach, savingsBase, savingsReach, untilRetirement;
 
-  //Set APR, in decimal form
-  var apr = .05;
   //Set Interest Compound Periods
   var ip = 12;
 
   function setValues(){
     value1 = Number($('#input-1').val());
     value2 = Number($('#input-2').val().replace('$',''));
-    value3 = Number($('#input-3').val().replace('$',''));
+    value3 = value2 * 1.2; // 20% greater monthly savings
     untilRetirement = 65 - value1;
-    $('#base-rate').html('$' + value2 + '/month');
-    $('#reach-rate').html('$' + value3 + '/month');
-    $('#result-1').html('$' + compoundInterest(0,apr,ip,untilRetirement,value2).formatMoney(2,'.',','));
-    $('#result-2').html('$' + compoundInterest(0,apr,ip,untilRetirement,value3).formatMoney(2,'.',','));
+    // yearsBase =
+    // yearsReach =
+    savingsBase = compoundInterest(0,apr,ip,untilRetirement,value2).formatMoney(0,'.',',');
+    savingsReach = compoundInterest(0,apr,ip,untilRetirement,value3).formatMoney(0,'.',',');
+
+    // Set summary stats
+    $('.summary-savings .base .big-number').html('$' + savingsBase);
+    $('.summary-savings .reach .big-number').html('$' + savingsReach);
+
+    // Set savings rates in summary stats
+    $('.summary-savings .base .rate').html('$' + value2);
+    $('.summary-savings .reach .rate').html('$' + value3.formatMoney(0,'.',','));
+    $('.reach-goal-explanation .rate.base').html('$' + value2);
+    $('.reach-goal-explanation .rate.reach').html('$' + value3.formatMoney(0,'.',','));
   }
 
-
-  // Multiply the values of the two sliders
-  $('#calculator-2, #calculator-3').change(function(){
-    $('#calculator-results .hidden').removeClass('hidden');
+  // Set the values and generate the graph every time you set input-2 value
+  $('#calculator-2').change(function(){
+    $('#calculator-results').removeClass('hidden');
     setValues();
     graphIt();
   });
 
-  // Function for compound interset
-  // @param P = initial deposit
-  // @param t = number of years the amount is deposited
-  //
-  // function compoundInterest(P, t){
-  //   // Compound interest
-  //   // A = amount of money accumulated after n years, including interest
-  //   // P = principal amount (the initial amount you borrow or deposit)
-  //   // r = annual rate of interest (as a decimal)
-  //   // n = number of times the interest is compounded per year
-  //   // t = number of years the amount is deposited
-  //   var A, r, t;
-  //   r = .02;
-  //   n = 12;
-  //   A = P*Math.pow((1 + r/n), n*t);
-  //   return A;
-  // }
+  //Set APR, in decimal form
+  var apr = .035;
+
+  // Set the values and generate graph every time you set input-3 value
+  $('#calculator-3 button').click(function(){
+      $('#calculator-3 .selected').removeClass('selected');
+      $(this).addClass('selected');
+      apr = $(this).data('apr');
+      setValues();
+      graphIt();
+  })
+
 
   // Chart.js implementation
   // http://www.chartjs.org/docs/#line-chart
-
 
   function compoundInterest(p,r,n,t,D){
     /*****************************
@@ -144,33 +124,37 @@ $(document).ready(function(){
 
     // Get context with jQuery - using jQuery's .get() method.
     var ctx = $("#projection").get(0).getContext("2d");
-    ctx.canvas.width = 800;
+    ctx.canvas.width = 450;
     ctx.canvas.height = 300;
     var options = {
       bezierCurve: true,
       scaleShowGridLines: false,
       pointDot: false,
       showTooltips: false,
-      scaleLabel : "<%= '$' + parseFloat(value).formatMoney(0,'.',',') %>"
+      scaleLabel : "<%= '$' + parseFloat(value).formatMoney(0,'.',',') %>",
+      // scaleOverride: true,
+      // scaleStepWidth: 100000,
+      // scaleStartValue: 0,
+      // scaleSteps: 5
     };
 
     var data = {
       labels: ages,
       datasets: [
           {
-              label: "Start saving later",
+              label: "Reach goal",
               fillColor: "#49B577",
               data: reachGoal,
           },
           {
-              label: "Start saving now",
+              label: "Base goal",
               fillColor: "#1297B5",
               data: baseGoal,
           },
 
         ]
     };
-    var projection = new Chart(ctx).Line(data, options);
+    var projection = new Chart(ctx).Bar(data, options);
   }
 
 
