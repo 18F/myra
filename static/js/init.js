d9ada8db9800bca89a8df39e7203c1b90f0a0458
Noah Manger <noah.manger@gsa.gov>
2014-10-08 16:06:41 -0700

    Small tweaks

--- static/js/init.js
+++ static/js/init.js
@@ -1,17 +1,6 @@
 // Quick and Easy Javascript Detection
 $("html").removeClass( "no-js" );
 
-// Navigation
-//// Add the mobile nav option
-$(".site-header").prepend('<button type="button" class="nav-toggle btn btn-default"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="sr-only">Toggle Navigation</span></button>');
-
-//// Activate nav on mobile nav toggle click
-$(".nav-toggle").click(function() {
-  $(".main-nav").toggleClass("active");
-  $(".site-title").toggle();
-});
-
-
 $(document).ready(function(){
 
   // Find and replace to italicize myRA
